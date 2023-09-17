943527aa057e0ffc9eaf379f329a1d05d6a6c966
Less <31947204+lessandsass@users.noreply.github.com>
2023-09-17 07:46:46 +0530

    Create event, can add data.
    create event form created, Validations were added on CreateEventRequest.php, Store method was updated in the EventController.php file.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,24 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Country;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\CreateEventRequest;
+use App\Models\Event;
 
 class EventController extends Controller
 {
-    public function index() :View
+    public function index() : View
     {
+
         return view('events.index');
     }
 
-    public function create()
+    public function create() : View
     {
-        //
+        $countries = Country::all();
+        return view('events.create', compact('countries'));
     }
 
-    public function store(Request $request)
+    public function store(CreateEventRequest $request)
     {
-        //
+        if ($request->hasFile('image')) {
+
+            $data = $request->validated();
+            $data['image'] = Storage::putFile('events', $request->file('image'));
+            $data['user_id'] = auth()->id();
+            $data['slug'] = Str::slug($request->title);
+
+            Event::create($data);
+            return to_route('events.index');
+
+        } else {
+            return back();
+        }
+
     }
 
     public function show(string $id)
---
+++ app/Http/Requests/CreateEventRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateEventRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|max:155,min:2',
+            'address' => 'required|max:155,min:2',
+            'image' => 'image|required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'start_time' => 'required',
+            'country_id' => 'required',
+            'city_id' => 'required',
+            'description' => 'required',
+            'num_tickets' => 'required',
+        ];
+    }
+}
