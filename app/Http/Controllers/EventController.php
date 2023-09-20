3eb1fe44c59d40b4ab8a29ee5711eb98ca358dee
Less <31947204+lessandsass@users.noreply.github.com>
2023-09-20 05:55:41 +0530

    view the data event data
    a tailwind table was created with particular fields. show the created event data.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,19 +9,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\CreateEventRequest;
 use App\Models\Event;
+use App\Models\Tag;
 
 class EventController extends Controller
 {
     public function index() : View
     {
 
-        return view('events.index');
+        $events = Event::with('country')->get();
+
+        return view('events.index', compact('events'));
     }
 
     public function create() : View
     {
         $countries = Country::all();
-        return view('events.create', compact('countries'));
+        $tags = Tag::all();
+
+        return view('events.create', compact('countries', 'tags'));
     }
 
     public function store(CreateEventRequest $request)
@@ -33,7 +38,8 @@ class EventController extends Controller
             $data['user_id'] = auth()->id();
             $data['slug'] = Str::slug($request->title);
 
-            Event::create($data);
+            $event = Event::create($data);
+            $event->tags()->attach($request->tags);
             return to_route('events.index');
 
         } else {
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -151,7 +151,6 @@
                             id="start_date"
                             name="start_date"
                             class="bg-gray-700 text-gray-300 text-sm rounded-lg border-transparent focus:border-transparent focus:ring-0"
-                            placeholder="Laravel event"
                         >
 
                         @error('start_date')
@@ -262,25 +261,28 @@
 
                     </div>
 
-                    {{-- <div>
+                    <div>
                         <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Tags</h3>
-
-                        <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg">
+                        <ul
+                            class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                             @foreach ($tags as $tag)
                                 <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                                     <div class="flex items-center pl-3">
-                                        <input type="checkbox" name="vue-checkbox-list" id="vue-checkbox-list" type="checkbox" name="tags[]" value="{{ $tag->id }}" class="w-4 h-4">
-                                        <label for="vue-checkbox-list" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                                        <input id="vue-checkbox-list" type="checkbox" name="tags[]"
+                                            value="{{ $tag->id }}"
+                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                        <label
+                                            class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ $tag->name }}</label>
                                     </div>
                                 </li>
                             @endforeach
                         </ul>
-                    </div> --}}
-
-                    <div>
-                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded px-4 py-2">Submit</button>
                     </div>
 
+                </div>
+
+                <div>
+                    <button type="submit" class="block text-white bg-blue-700 hover:bg-blue-800 font-medium rounded px-4 py-2">Submit</button>
                 </div>
 
             </form>
